a623d07369971eef6eeda848db5f5d545cd5f188
Jamie Prentice <a.jamie.prentice@gmail.com>
2017-09-11 00:36:59 +0100

    Better console output

--- benchmark.sh
+++ benchmark.sh
@@ -3,29 +3,36 @@
 # Don't forget to "chmod +x benchmarks.sh" this file
 
 echo "Starting Benchmark Scripts"
+echo "Installing Sysbench..."
 
-apt -qq install sysbench -y & wait
+apt install -qq sysbench -y & wait
 
+echo "Running CPU Benchmarks... (This could take a while)"
 echo "------ CPU ------" >> results.txt
 sysbench --test=cpu run >> results.txt & wait
 
+echo "Running Memory Read Benchmarks..."
 echo "------ Memory Read ------" >> results.txt
 sysbench --test=memory run >> results.txt & wait
 
+echo "Running Memory Write Benchmarks..."
 echo "------ Memory Write ------" >> results.txt
 sysbench --test=memory --memory-oper=write run >> results.txt & wait
 
-echo "------ DISK IO ------" >> results.txt
+echo "Running Disk I/O Benchmarks..."
+echo "------ DISK I/O ------" >> results.txt
 sysbench --test=fileio prepare & wait
 sysbench --test=fileio --file-test-mode=rndrw run >> results.txt & wait
 sysbench --test=fileio cleanup & wait
 
-echo "------ NETWORK ------" >> results.txt
+echo "Installing Python and Speedtest-CLI..."
 apt install python-pip -y & wait
 pip install speedtest-cli & wait
+echo "Running Memory Read Benchmarks..."
+echo "------ NETWORK ------" >> results.txt
 speedtest-cli >> results.txt & wait
 
-echo "Uninstalling Packages"
+echo "Uninstalling packages & clearing up"
 apt remove sysbench -y & wait
 apt remove python-pip -y & wait
 apt clean -y & wait
