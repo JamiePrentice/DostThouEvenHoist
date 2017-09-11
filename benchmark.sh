29582e8aae12c7ba99cc08c96441aa7692243735
Jamie Prentice <a.jamie.prentice@gmail.com>
2017-09-11 01:27:35 +0100

    Shhh python it's okay + Newlines in textile.

--- benchmark.sh
+++ benchmark.sh
@@ -11,45 +11,45 @@ echo -e "${YELLOW}[LazyBench]${CLEAR} - Installing Sysbench..."
 apt install -qq sysbench -y & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Running CPU Benchmarks..."
-echo "------ CPU ------" >> results.txt
+echo "\n----- CPU ------\n" >> results.txt
 sysbench --test=cpu run >> results.txt & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Memory Capacity:"
 lshw -short -C memory
 
-echo "------ Memory ------" >> results.txt
+echo "\n----- Memory ------\n" >> results.txt
 lshw -short -C memory >> results.txt
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Memory Read Benchmarks... (This could take a while)"
-echo "------ Memory Read ------" >> results.txt
+echo "\n----- Memory Read ------\n" >> results.txt
 sysbench --test=memory run >> results.txt & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Memory Write Benchmarks... (This could also take a while)"
-echo "------ Memory Write ------" >> results.txt
+echo "\n----- Memory Write ------\n" >> results.txt
 sysbench --test=memory --memory-oper=write run >> results.txt & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Disk Capacity:"
 df -h
-echo "------ Disk Capacity ------" >> results.txt
+echo "\n----- Disk Capacity ------\n" >> results.txt
 df -h >> results.txt & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Disk I/O Benchmarks..."
-echo "------ DISK I/O ------" >> results.txt
+echo "\n----- DISK I/O ------\n" >> results.txt
 sysbench --test=fileio prepare & wait
 sysbench --test=fileio --file-test-mode=rndrw run >> results.txt & wait
 sysbench --test=fileio cleanup & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Installing Python and Speedtest-CLI..."
-apt install python-pip -y & wait
-pip install speedtest-cli & wait
+apt install -qq python-pip -y & wait
+pip install -qq speedtest-cli & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Network Benchmarks..."
-echo "------ NETWORK ------" >> results.txt
+echo "\n----- NETWORK ------\n" >> results.txt
 speedtest-cli >> results.txt & wait
 
 echo -e "${YELLOW}[LazyBench]${CLEAR} - Uninstalling packages & clearing up"
-apt remove sysbench -y & wait
-apt remove python-pip -y & wait
+apt remove -qq sysbench -y & wait
+apt remove -qq python-pip -y & wait
 apt clean -y & wait
 apt autoremove -y & wait
 
