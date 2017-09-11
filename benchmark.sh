54d0f3d06a54a04c470b5f3589a85e29e5280871
JamiePrentice <a.jamie.prentice@gmail.com>
2017-09-11 08:42:44 +0100

    Bash variables. I'm slowly descovering the wonderful world of bash scripts.

--- benchmark.sh
+++ benchmark.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
-
-YELLOW='\033[1;33m'
-CLEAR='\033[0m' # No Color
+LazyBench='\033[1;33m[LazyBench]\033[0m -'
 
 # Don't forget to "chmod +x benchmarks.sh" this file
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Starting Benchmark Scripts"
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Installing Sysbench..."
+echo -e "$LazyBench Starting Benchmark Scripts"
+echo -e "$LazyBench Installing Sysbench..."
 
 apt install sysbench -y & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Running CPU Benchmarks..."
+echo -e "$LazyBench Running CPU Benchmarks..."
 echo "\n----- CPU ------\n" >> results.txt
 sysbench --test=cpu run >> results.txt & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Memory Capacity:"
+echo -e "$LazyBench Memory Capacity:"
 lshw -short -C memory
 
 echo "\n----- Memory ------\n" >> results.txt
 lshw -short -C memory >> results.txt
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Memory Read Benchmarks... (This could take a while)"
+echo -e "$LazyBench Running Memory Read Benchmarks... (This could take a while)"
 echo "\n----- Memory Read ------\n" >> results.txt
 sysbench --test=memory run >> results.txt & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Memory Write Benchmarks... (This could also take a while)"
+echo -e "$LazyBench Running Memory Write Benchmarks... (This could also take a while)"
 echo "\n----- Memory Write ------\n" >> results.txt
 sysbench --test=memory --memory-oper=write run >> results.txt & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Disk Capacity:"
+echo -e "$LazyBench Disk Capacity:"
 df -h
 echo "\n----- Disk Capacity ------\n" >> results.txt
 df -h >> results.txt & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Disk I/O Benchmarks..."
+echo -e "$LazyBench Running Disk I/O Benchmarks..."
 echo "\n----- DISK I/O ------\n" >> results.txt
 sysbench --test=fileio prepare & wait
 sysbench --test=fileio --file-test-mode=rndrw run >> results.txt & wait
 sysbench --test=fileio cleanup & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Installing Python and Speedtest-CLI..."
+echo -e "$LazyBench Installing Python and Speedtest-CLI..."
 apt install python-pip -y & wait
 pip install speedtest-cli & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Running Network Benchmarks..."
+echo -e "$LazyBench Running Network Benchmarks..."
 echo "\n----- NETWORK ------\n" >> results.txt
 echo "\n Network test 1: \n" >> results.txt
 speedtest-cli >> results.txt & wait
@@ -56,10 +54,10 @@ speedtest-cli >> results.txt & wait
 echo "\n Network test 5: \n" >> results.txt
 speedtest-cli >> results.txt & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Uninstalling packages & clearing up"
+echo -e "$LazyBench Uninstalling packages & clearing up"
 apt remove sysbench -y & wait
 apt remove python-pip -y & wait
 apt clean -y & wait
 apt autoremove -y & wait
 
-echo -e "${YELLOW}[LazyBench]${CLEAR} - Done - Results in results.txt"
+echo -e "$LazyBench Done - Results in results.txt"
