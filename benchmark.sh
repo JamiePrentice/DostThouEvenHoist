103e08a98b929fbbd3297b0142d25d46ecf5b60c
Jamie Prentice <a.jamie.prentice@gmail.com>
2017-09-11 23:03:08 +0100

    Removed multiple network tests - no need for that.

--- benchmark.sh
+++ benchmark.sh
@@ -12,9 +12,9 @@ pip install speedtest-cli & wait
 
 clear
 
-printf "$LazyBench Starting Benchmark Scripts"
+printf "$LazyBench Starting Benchmark Scripts... \n"
 
-printf "$LazyBench Running CPU Benchmarks... \n"
+printf "$LazyBench Running CPU Benchmarks... \n\n"
 printf "\n----- CPU ------\n\n" >> results.txt
 cat /proc/cpuinfo | grep 'model name' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cpu cores'  2>&1 | tee -a results.txt
@@ -22,15 +22,15 @@ cat /proc/cpuinfo | grep 'cpu MHz' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cache size' 2>&1 | tee -a results.txt
 sysbench --test=cpu run >> results.txt & wait
 
-printf "$LazyBench Memory Capacity: \n"
+printf "$LazyBench Memory Capacity: \n\n"
 printf "\n----- Memory ------\n\n" >> results.txt
 lshw -short -C memory 2>&1 | tee -a results.txt
 
-printf "$LazyBench Running Memory Read Benchmarks... (This could take a while)"
+printf "$LazyBench Running Memory Read Benchmarks... (This could take a while) \n"
 printf "\n----- Memory Read ------\n\n" >> results.txt
 sysbench --test=memory run >> results.txt & wait
 
-printf "$LazyBench Running Memory Write Benchmarks... (This could also take a while)"
+printf "$LazyBench Running Memory Write Benchmarks... (This could also take a while) \n"
 printf "\n----- Memory Write ------\n\n" >> results.txt
 sysbench --test=memory --memory-oper=write run >> results.txt & wait
 
@@ -46,15 +46,6 @@ sysbench --test=fileio cleanup & wait
 
 printf "$LazyBench Running Network Benchmarks..."
 printf "----- NETWORK ------\n\n" >> results.txt
-printf "Network test 1: \n\n" >> results.txt
-speedtest-cli >> results.txt & wait
-printf "Network test 2: \n\n" >> results.txt
-speedtest-cli >> results.txt & wait
-printf "Network test 3: \n\n" >> results.txt
-speedtest-cli >> results.txt & wait
-printf "Network test 4: \n\n" >> results.txt
-speedtest-cli >> results.txt & wait
-printf "Network test 5: \n\n" >> results.txt
 speedtest-cli >> results.txt & wait
 
 printf "$LazyBench Uninstalling packages & clearing up \n"
@@ -62,6 +53,5 @@ apt remove sysbench -y & wait
 apt remove python-pip -y & wait
 apt clean -y & wait
 apt autoremove -y & wait
-rm blackhole.txt & wait
 
 printf "$LazyBench Done - Results in results.txt \n"
