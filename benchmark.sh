085f28c2726a9e1a272760ed6e025a71b1ba65ee
Jamie Prentice <a.jamie.prentice@gmail.com>
2017-09-11 21:59:36 +0100

    Newlines

--- benchmark.sh
+++ benchmark.sh
@@ -8,7 +8,7 @@ printf "$LazyBench Starting Benchmark Scripts"
 printf "$LazyBench Installing Sysbench..."
 apt install sysbench -y & wait
 
-printf "$LazyBench Running CPU Benchmarks..."
+printf "$LazyBench Running CPU Benchmarks... \n"
 printf "\n----- CPU ------\n\n" >> results.txt
 cat /proc/cpuinfo | grep 'model name' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cpu cores'  2>&1 | tee -a results.txt
@@ -16,7 +16,7 @@ cat /proc/cpuinfo | grep 'cpu MHz' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cache size' 2>&1 | tee -a results.txt
 sysbench --test=cpu run >> results.txt & wait
 
-printf "$LazyBench Memory Capacity:"
+printf "$LazyBench Memory Capacity: \n"
 printf "\n----- Memory ------\n\n" >> results.txt
 lshw -short -C memory 2>&1 | tee -a results.txt
 
@@ -28,7 +28,7 @@ printf "$LazyBench Running Memory Write Benchmarks... (This could also take a wh
 printf "\n----- Memory Write ------\n\n" >> results.txt
 sysbench --test=memory --memory-oper=write run >> results.txt & wait
 
-printf "$LazyBench Disk Capacity:"
+printf "$LazyBench Disk Capacity: \n"
 printf "\n----- Disk Capacity ------\n\n" >> results.txt
 df -h 2>&1 | tee -a results.txt & wait
 
@@ -56,11 +56,11 @@ speedtest-cli >> results.txt & wait
 printf "Network test 5: \n\n" >> results.txt
 speedtest-cli >> results.txt & wait
 
-printf "$LazyBench Uninstalling packages & clearing up"
+printf "$LazyBench Uninstalling packages & clearing up \n"
 apt remove sysbench -y & wait
 apt remove python-pip -y & wait
 apt clean -y & wait
 apt autoremove -y & wait
 rm blackhole.txt & wait
 
-printf "$LazyBench Done - Results in results.txt"
+printf "$LazyBench Done - Results in results.txt \n"
