7e54a75fb3cf66cbf7118d5f78145559d89acfde
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2018-05-09 17:41:46 +0100

    Sysbench --test is not depreciated

--- benchmark.sh
+++ benchmark.sh
@@ -23,8 +23,8 @@ cat /proc/cpuinfo | grep 'model name' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cpu cores'  2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cpu MHz' 2>&1 | tee -a results.txt
 cat /proc/cpuinfo | grep 'cache size' 2>&1 | tee -a results.txt
-sysbench --test=cpu --cpu-max-prime=25000 run >> results.txt & wait
-sysbench --test=cpu --cpu-max-prime=25000 run --num-threads=4 >> results.txt & wait
+sysbench cpu --cpu-max-prime=25000 run >> results.txt & wait
+sysbench cpu --cpu-max-prime=25000 run --num-threads=4 >> results.txt & wait
 
 printf "$LazyBench Memory Capacity: \n\n"
 printf "\n######## Memory ########\n\n" >> results.txt
@@ -32,11 +32,11 @@ lshw -short -C memory 2>&1 | tee -a results.txt
 
 printf "$LazyBench Running Memory Read Benchmarks... (This could take a while) \n"
 printf "\n######## Memory Read ########\n\n" >> results.txt
-sysbench --test=memory --memory-oper=read run >> results.txt & wait
+sysbench memory --memory-oper=read run >> results.txt & wait
 
 printf "$LazyBench Running Memory Write Benchmarks... (This could also take a while) \n"
 printf "\n######## Memory Write ########\n\n" >> results.txt
-sysbench --test=memory --memory-oper=write run >> results.txt & wait
+sysbench memory --memory-oper=write run >> results.txt & wait
 
 printf "$LazyBench Disk Capacity: \n\n"
 printf "\n######## Disk Capacity ########\n\n" >> results.txt
@@ -44,9 +44,9 @@ df -h 2>&1 | tee -a results.txt & wait
 
 printf "$LazyBench Running Disk I/O Benchmarks..."
 printf "\n######## DISK I/O ########\n\n" >> results.txt
-sysbench --test=fileio prepare & wait
-sysbench --test=fileio --file-test-mode=rndrw run >> results.txt & wait
-sysbench --test=fileio cleanup & wait
+sysbench fileio prepare & wait
+sysbench -fileio --file-test-mode=rndrw run >> results.txt & wait
+sysbench fileio cleanup & wait
 
 printf "$LazyBench Running Network Benchmarks...\n"
 printf "\n######## NETWORK ########\n\n" >> results.txt
